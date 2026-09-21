34d720cb1265d785b7dc99d7d78504eef9539ff5
opra <@trezz325>
2026-09-21 08:38:41 +0300

    solution add

--- task1/test1.c
+++ task1/test1.c
@@ -5,8 +5,8 @@ int ost(int a, int b, int n);
 
 int main(void){
     assert(ost(5, 1, 2) == 2);
-assert(ost(10, 50, 3) == 50);
-assert(ost(1, 25, 4) == 0);
+    assert(ost(10, 50, 3) == 50);
+    assert(ost(1, 25, 4) == 0);
     printf("All tests passed!\n");
     
 }
